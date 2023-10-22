12b849a360a27f23ef4706ccb41047096fbcc0aa
Robert <144385902+Rob2408@users.noreply.github.com>
2023-10-22 16:53:59 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 import cv2
 import pytesseract
+import gi
 
-cap = cv2.VideoCapture(0)  # Use the default camera (change the index if you have multiple cameras)
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+
+# Initialize GStreamer
+Gst.init(None)
+
+# Create a GStreamer pipeline to capture video from the default camera
+pipeline = Gst.parse_launch("v4l2src ! videoconvert ! appsink")
+
+# Create an OpenCV VideoCapture object with the pipeline
+cap = cv2.VideoCapture(pipeline.get_property('name'), cv2.CAP_GSTREAMER)
 
 while True:
     ret, frame = cap.read()  # Capture a frame
@@ -21,27 +32,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-
-# import tkinter as tk
-
-# # Run using: python3 test.py
-# # Run to install tkinter on Pi: sudo apt-get install python3-tk
-
-# def display_text():
-#     # Create a new tkinter window
-#     window = tk.Tk()
-
-#     # Set the window title
-#     window.title("Raspberry Pi Display")
-
-#     # Create a label with your text
-#     label = tk.Label(window, text="Hello, Raspberry Pi 4!")
-
-#     # Add the label to the window
-#     label.pack()
-
-#     # Run the tkinter event loop
-#     window.mainloop()
-
-# # Call the function to display the text
-# display_text()
