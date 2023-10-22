88138beb82fa1642b261bc53486008d58ecc508e
Robert <144385902+Rob2408@users.noreply.github.com>
2023-10-22 16:56:03 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,34 +1,51 @@
-import cv2
-import pytesseract
 import gi
-
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
+import cv2
+import pytesseract
+import numpy as np
 
 # Initialize GStreamer
 Gst.init(None)
 
-# Create a GStreamer pipeline to capture video from the default camera
-pipeline = Gst.parse_launch("v4l2src ! videoconvert ! appsink")
+# Create a GStreamer pipeline for video capture
+pipeline = Gst.parse_launch("v4l2src ! videoconvert ! video/x-raw,format=BGR ! appsink")
 
-# Create an OpenCV VideoCapture object with the pipeline
-cap = cv2.VideoCapture(pipeline.get_property('name'), cv2.CAP_GSTREAMER)
+# Set up a callback function to process each frame
+def on_new_sample(appsink):
+    sample = appsink.emit("pull-sample")
+    buffer = sample.get_buffer()
+    success, frame = buffer.extract_dup(0, buffer.get_size())
+    
+    if success:
+        # Use Tesseract to perform OCR on the frame
+        frame = cv2.imdecode(np.frombuffer(frame, dtype=np.uint8), cv2.IMREAD_COLOR)
+        detected_text = pytesseract.image_to_string(frame)
 
-while True:
-    ret, frame = cap.read()  # Capture a frame
+        # Display the detected text on the frame
+        cv2.putText(frame, detected_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    # Use Tesseract to perform OCR on the frame
-    detected_text = pytesseract.image_to_string(frame)
+        # Display the frame with detected text
+        cv2.imshow("Text Detection", frame)
+    
+    return Gst.FlowReturn.OK
 
-    # Display the detected text on the frame
-    cv2.putText(frame, detected_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+# Configure the appsink element to call the callback function
+appsink = pipeline.get_by_name("appsink0")
+appsink.set_property("emit-signals", True)
+appsink.connect("new-sample", on_new_sample)
 
-    # Display the frame with detected text
-    cv2.imshow("Text Detection", frame)
+# Start the GStreamer pipeline
+pipeline.set_state(Gst.State.PLAYING)
 
-    # Exit the loop when the 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+# Main loop
+try:
+    while True:
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+except KeyboardInterrupt:
+    pass
+finally:
+    # Stop the GStreamer pipeline
+    pipeline.set_state(Gst.State.NULL)
+    cv2.destroyAllWindows()
