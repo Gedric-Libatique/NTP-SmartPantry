09a98900e17ea9f85d4cc8f49bf6ade710479e3d
Robert <144385902+Rob2408@users.noreply.github.com>
2023-10-31 22:28:21 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,50 @@
-# sudo modprobe bcm2835-v4l2
+import pytesseract
+from pytesseract import Output
+from picamera2 import Picamera2
 import cv2
+import numpy as np
+import time
 
-# Initialize the camera
-cap = cv2.VideoCapture(0)
+# Initialize PiCamera
+camera = Picamera2()
 
-# Check if the camera opened successfully
-if not cap.isOpened():
-    print("Error opening camera")
+# Set camera resolution (adjust these values as needed)
+camera.resolution = (1280, 720)
+capture_config = camera.create_still_configuration()
+camera.start()
 
-# Capture and display video frames until 'q' is pressed
 while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    # Capture a frame from the camera feed
+    frame = camera.capture_frame()
 
-    # If frame is read correctly, ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
+    # Display the live feed in a window
+    cv2.imshow('Camera Feed', frame)
+
+    # Wait for user input and capture an image if 'p' is pressed
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('p'):
+        # Capture an image and save it to a file
+        camera.capture_file("image.jpg")
         break
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
+# When everything is done, release the camera and close the windows
+camera.close()
+cv2.destroyAllWindows()
 
-    # Break the loop on 'q' key press
-    if cv2.waitKey(1) == ord('q'):
-        break
+# Read the captured image from file
+frame = cv2.imread('image.jpg')
 
-# When everything done, release the capture and destroy all windows
-cap.release()
+d = pytesseract.image_to_data(frame, output_type=Output.DICT)
+n_boxes = len(d['text'])
+for i in range(n_boxes):
+    if int(d['conf'][i]) > 60:
+        (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+        # don't show empty text
+        if text and text.strip() != "":
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            frame = cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+
+# Display the resulting frame
+cv2.imshow('frame', frame)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
