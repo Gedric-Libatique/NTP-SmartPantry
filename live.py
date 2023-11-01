e3d159d25a3b95889f3880164ee914d67c47b5cd
Robert <144385902+Rob2408@users.noreply.github.com>
2023-10-31 18:42:23 -0700

    Update live.py

--- live.py
+++ live.py
@@ -1,26 +1,45 @@
-import cv2
 import pytesseract
 from pytesseract import Output
-import picamera2
+from picamera2 import Picamera2
+import io
+import cv2
+import numpy as np
+import time
 
-with picamera2.PiCamera() as camera:
-    while True:
-        # Capture frame-by-frame
-        frame = camera.capture()
+# Initialize PiCamera
+camera = Picamera2()
 
-        d = pytesseract.image_to_data(frame, output_type=Output.DICT)
-        n_boxes = len(d['text'])
-        for i in range(n_boxes):
-            if int(d['conf'][i]) > 60:
-                (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-                # don't show empty text
-                if text and text.strip() != "":
-                    frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                    frame = cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+# Set camera resolution (adjust these values as needed)
+camera.resolution = (1280, 720)
 
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+# Create an in-memory stream for capturing the image
+stream = io.BytesIO()
 
+while True:
+    # Capture an image and save it to the stream
+    camera.capture(stream, format='jpeg')
+    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    frame = cv2.imdecode(data, 1)
+
+    # Reset the stream for the next capture
+    stream.seek(0)
+    stream.truncate()
+
+    d = pytesseract.image_to_data(frame, output_type=Output.DICT)
+    n_boxes = len(d['text'])
+    for i in range(n_boxes):
+        if int(d['conf'][i]) > 60:
+            (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            # don't show empty text
+            if text and text.strip() != "":
+                frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                frame = cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the camera
+camera.close()
 cv2.destroyAllWindows()
