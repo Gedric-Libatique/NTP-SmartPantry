a6548238a3a7918530ccaf2c793ae191c499d1c5
Robert <144385902+Rob2408@users.noreply.github.com>
2023-10-31 21:54:49 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,30 @@
-# global cap_v4l.cpp:1119 tryIoctl VIDEOIO(V4L2:/dev/video0): select() timeout.
 # sudo modprobe bcm2835-v4l2
-# import cv2
-from cv2 import cv2
+import cv2
 
-camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-status, image = camera.read()
+# Initialize the camera
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
-# Capture frame
-ret, frame = cap.read()
-if ret:
-	cv2.imwrite('testimage.jpg', frame)
+# Check if the camera opened successfully
+if not cap.isOpened():
+    print("Error opening camera")
 
+# Capture and display video frames until 'q' is pressed
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # If frame is read correctly, ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+
+    # Break the loop on 'q' key press
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# When everything done, release the capture and destroy all windows
 cap.release()
+cv2.destroyAllWindows()
