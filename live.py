617ed1c4300e53afa374f19fe8b3e53c59bb87a1
Robert <144385902+Rob2408@users.noreply.github.com>
2023-10-31 18:35:37 -0700

    Update live.py

--- live.py
+++ live.py
@@ -1,14 +1,29 @@
-import cv2
 import pytesseract
 from pytesseract import Output
- 
-cap = cv2.VideoCapture(0)
-# cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
- 
+from picamera import PiCamera
+import io
+import cv2
+import numpy as np
+
+# Initialize PiCamera
+camera = PiCamera()
+
+# Set camera resolution (adjust these values as needed)
+camera.resolution = (1280, 720)
+
+# Create an in-memory stream for capturing the image
+stream = io.BytesIO()
+
 while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
- 
+    # Capture an image and save it to the stream
+    camera.capture(stream, format='jpeg')
+    data = np.fromstring(stream.getvalue(), dtype=np.uint8)
+    frame = cv2.imdecode(data, 1)
+
+    # Reset the stream for the next capture
+    stream.seek(0)
+    stream.truncate()
+
     d = pytesseract.image_to_data(frame, output_type=Output.DICT)
     n_boxes = len(d['text'])
     for i in range(n_boxes):
@@ -18,12 +33,12 @@ while True:
             if text and text.strip() != "":
                 frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
                 frame = cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
- 
+
     # Display the resulting frame
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
- 
-# When everything done, release the capture
-cap.release()
+
+# When everything is done, release the camera
+camera.close()
 cv2.destroyAllWindows()
