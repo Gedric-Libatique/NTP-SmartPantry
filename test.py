66352771add30f9380dea6d3950dfcb70387572e
Robert <144385902+Rob2408@users.noreply.github.com>
2023-10-31 20:19:25 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,19 @@
 import cv2
 
-def gstreamer_pipeline(device, capture_width, capture_height, framerate, display_width, display_height):
-    return (
-        f'v4l2src device=/dev/video{device} ! '
-        f'video/x-raw, width={capture_width}, height={capture_height}, framerate={framerate}/1 ! '
-        'videoconvert ! videoscale ! '
-        f'video/x-raw, width={display_width}, height={display_height} ! appsink'
-    )
+# Open the camera
+cap = cv2.VideoCapture(0)
 
-# Pipeline parameters
-capture_width = 1280
-capture_height = 720
-display_width = 640
-display_height = 360
-framerate = 30
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 
-pipeline = gstreamer_pipeline(0, capture_width, capture_height, framerate, display_width, display_height)
-cap = cv2.VideoCapture(pipeline, cv2.CAP_GSTREAMER)
+    # Display the resulting frame
+    cv2.imshow('Live Video', frame)
 
-if not cap.isOpened():
-    print("Failed to open camera.")
-else:
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        
-        # Process the frame here if needed
-        
-        cv2.imshow('Camera Feed', frame)
-        
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    # Break the loop on 'Q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    cap.release()
-    cv2.destroyAllWindows()
+# When everything done, release the capture and destroy all windows
+cap.release()
+cv2.destroyAllWindows()
