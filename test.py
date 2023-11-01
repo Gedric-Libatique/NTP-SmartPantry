d51d1e4def29e60feb6ad54009e6a7052a0be4e2
Robert <144385902+Rob2408@users.noreply.github.com>
2023-10-31 21:49:32 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 # global cap_v4l.cpp:1119 tryIoctl VIDEOIO(V4L2:/dev/video0): select() timeout.
 # sudo modprobe bcm2835-v4l2
-import cv2
+# import cv2
+from cv2 import cv2
 
+camera.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+status, image = camera.read()
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 
 # Capture frame
 ret, frame = cap.read()
