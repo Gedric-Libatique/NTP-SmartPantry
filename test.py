c9c035c93341a58492ec364933bf18bab776bd2d
Robert <144385902+Rob2408@users.noreply.github.com>
2023-11-02 22:09:55 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,37 +1,57 @@
 import cv2
-import numpy as np
 import pytesseract
+from pytesseract import Output
+import threading
 
-# Set the path to the tesseract executable
-pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+# Create a lock to prevent simultaneous thread access to the frame
+frame_lock = threading.Lock()
 
-# Start video capture
+# Start video capture on a separate thread
+def capture():
+    global cap, frame
+    while True:
+        ret, f = cap.read()
+        with frame_lock:
+            frame = f
+
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
+# Start the capture thread
+cap_thread = threading.Thread(target=capture)
+cap_thread.start()
+
+frame_count = 0
 while True:
-    # Read frame by frame
-    ret, frame = cap.read()
-    
-    # Convert the image from BGR to RGB
-    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    
-    # Use Tesseract to find text in the image
-    data = pytesseract.image_to_data(rgb, output_type=pytesseract.Output.DICT)
-    
-    # Iterate over each "word" that Tesseract found
-    for i in range(len(data['text'])):
-        # If the word is not an empty string, draw a rectangle around it
-        if int(data['conf'][i]) > 60:
-            (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
-            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    
-    # Break the loop on 'q' key press
+    # Capture frame-by-frame
+    with frame_lock:
+        f = frame.copy()
+
+    # Skip frames to improve FPS
+    frame_count += 1
+    if frame_count % 5 == 0:
+        # Resize the frame
+        f = cv2.resize(f, None, fx=0.5, fy=0.5)
+
+        # Convert to grayscale
+        gray = cv2.cvtColor(f, cv2.COLOR_BGR2GRAY)
+
+        # Use Tesseract to find text in the image
+        d = pytesseract.image_to_data(gray, output_type=Output.DICT)
+        n_boxes = len(d['text'])
+        for i in range(n_boxes):
+            if int(d['conf'][i]) > 60:
+                (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+                # don't show empty text
+                if text and text.strip() != "":
+                    f = cv2.rectangle(f, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    f = cv2.putText(f, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+
+        # Display the resulting frame
+        cv2.imshow('frame', f)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the capture and destroy all windows when done
+# When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
