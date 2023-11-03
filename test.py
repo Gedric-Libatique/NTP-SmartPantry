718f544ad8c5ee73e0177c96d6168182d3f1eb96
Robert <144385902+Rob2408@users.noreply.github.com>
2023-11-02 22:06:20 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,40 +1,37 @@
-import pytesseract
-from pytesseract import Output
-from picamera2 import Picamera2, Preview
-import numpy as np
 import cv2
-import time
-import keyboard
+import numpy as np
+import pytesseract
 
-camera = Picamera2()
-camera_config = camera.create_preview_configuration()
-camera.configure(camera_config)
-camera.start_preview(Preview.QTGL)
-camera.start()
+# Set the path to the tesseract executable
+pytesseract.pytesseract.tesseract_cmd = '/usr/bin/tesseract'
+
+# Start video capture
+cap = cv2.VideoCapture(0)
 
 while True:
-    # Wait for user input and capture an image if 'p' is pressed
-    if keyboard.is_pressed('p'):
-        # Capture an image and save it to a file
-        camera.capture_file("image.jpg")
+    # Read frame by frame
+    ret, frame = cap.read()
+    
+    # Convert the image from BGR to RGB
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    
+    # Use Tesseract to find text in the image
+    data = pytesseract.image_to_data(rgb, output_type=pytesseract.Output.DICT)
+    
+    # Iterate over each "word" that Tesseract found
+    for i in range(len(data['text'])):
+        # If the word is not an empty string, draw a rectangle around it
+        if int(data['conf'][i]) > 60:
+            (x, y, w, h) = (data['left'][i], data['top'][i], data['width'][i], data['height'][i])
+            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    
+    # Break the loop on 'q' key press
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything is done, release the camera and close the windows
-camera.close()
-time.sleep(2)
-frame = cv2.imread('image.jpg')
-
-d = pytesseract.image_to_data(frame, output_type=Output.DICT)
-n_boxes = len(d['text'])
-for i in range(n_boxes):
-    if int(d['conf'][i]) > 60:
-        (text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-        # don't show empty text
-        if text and text.strip() != "":
-            frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            frame = cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
-
-# Display the resulting frame
-cv2.imshow('frame', frame)
-cv2.waitKey(0)
+# Release the capture and destroy all windows when done
+cap.release()
 cv2.destroyAllWindows()
