72b1c4a44617782933ef8517864ff02da3e5c2ae
Robert <144385902+Rob2408@users.noreply.github.com>
2023-11-03 21:10:18 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,7 +1,8 @@
 import tkinter as tk
 from tkinter import filedialog
-from PIL import Image, ImageTk  # Pillow library for image display
+import cv2
 import os
+import threading
 
 class Application(tk.Frame):
     def __init__(self, master=None):
@@ -11,7 +12,8 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.quit = tk.Button(self, text="Exit", fg="red", command=self.master.destroy)
+        self.quit = tk.Button(self, text="Exit", fg="red",
+                              command=self.master.destroy)
         self.quit.pack(side="top", anchor="ne")
 
         self.cam_button = tk.Button(self, text="Enable Camera", command=self.enable_camera)
@@ -37,21 +39,8 @@ class Application(tk.Frame):
         image_dir = filedialog.askdirectory()
         image_files = [f for f in os.listdir(image_dir) if f.endswith('.jpg')]
 
-        if not image_files:
-            print("No .jpg images found in the directory.")
-            return
-
-        image_list_window = tk.Toplevel(self.master)
-        image_list_window.title("Image List")
-
-        for image_file in image_files:
-            image_path = os.path.join(image_dir, image_file)
-            img = Image.open(image_path)
-            img = img.resize((200, 200), Image.ANTIALIAS)
-            img = ImageTk.PhotoImage(img)
-            label = tk.Label(image_list_window, image=img, text=image_file)
-            label.image = img
-            label.pack()
+        for image in image_files:
+            print(image)
 
 root = tk.Tk()
 app = Application(master=root)
