9f59d2628f9e1b427b679aff6ede682771aa2c22
Robert <144385902+Rob2408@users.noreply.github.com>
2023-11-03 21:45:57 -0700

    Update gui.py

--- gui.py
+++ gui.py
@@ -27,7 +27,19 @@ class Application(tk.Frame):
 
         while True:
             ret, frame = cap.read()
-            cv2.imshow('Camera Feed', frame)
+			d = pytesseract.image_to_data(frame, output_type=Output.DICT)
+			n_boxes = len(d['text'])
+			
+			for i in range(n_boxes):
+				if int(d['conf'][i]) > 60:
+					(text, x, y, w, h) = (d['text'][i], d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+					# don't show empty text
+					if text and text.strip() != "":
+						frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+						frame = cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
+
+			# Display the resulting frame
+			cv2.imshow('frame', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
